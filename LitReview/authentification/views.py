766e5f3d70ef8f6e870ae7bf2a60d2ac9553ced5
Denizot <denizot.myriam@gmail.com>
2022-12-08 10:43:14 +0100

    Page de connexion finis css inclus

---
+++ LitReview/authentification/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
+# import des fonctions login et authenticate
+from . import forms
+from authentification import forms
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        message = 'Identifiants invalides.'
+    return render(request, 'authentification/login.html', context={'form': form, 'message': message})
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentification/signup.html', context={'form': form})
